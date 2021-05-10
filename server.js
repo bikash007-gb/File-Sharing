5515a5656bd2eaea47dd0d8f35c3ffe290f85524
bikash007-gb <bikashmali12@gmail.com>
2021-05-10 20:46:59 +0530

    cors

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const express = require('express')
 const path = require('path')
+const cors = require('cors')
 const app = express()
 app.use(express.json())
 const port = process.env.PORT || 3000
 app.use(express.static('public'))
 const connectDB  = require('./config/db')
 connectDB()
+
+//cors
+const corsOptions ={
+    origin:process.env.ALLOWED_CLIENTS.split(',')
+
+}
+app.use(cors(corsOptions))
 //Templates
 app.set('views',path.join(__dirname,'/views'))
 app.set('view engine','ejs')
